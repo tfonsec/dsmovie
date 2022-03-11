e69914e80187707d5d02253066697b399bfcb7c4
Thiago Fonseca <thifonseca85@gmail.com>
2022-03-11 18:57:53 -0300

    refactored NavBar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -13,7 +13,7 @@ function Navbar() {
                 <a href="https://github.com/tfonsec" target="_blank" rel="noreferrer">
                     <div className="dsmovie-contact-container">
                         <GithubIcon />
-                        <p className="dsmovie-contact-link">/devsuperior</p>
+                        <p className="dsmovie-contact-link">/Thiago Fonseca</p>
                     </div>
                 </a>
             </div>
